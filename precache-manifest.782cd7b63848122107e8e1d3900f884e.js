3f285c3284cc7537dc18a5310323798510a920f8
Aniket <aniket111awa@gmail.com>
2020-10-15 11:04:59 +0530

    Updates

---
+++ precache-manifest.782cd7b63848122107e8e1d3900f884e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "128718082ef8fb5e4a5af3b0c8da2403",
+    "url": "/MyResume/index.html"
+  },
+  {
+    "revision": "34b5d3882cda677bcd47",
+    "url": "/MyResume/static/css/main.6fd10c1f.chunk.css"
+  },
+  {
+    "revision": "aba9cdfafa3c6a59c137",
+    "url": "/MyResume/static/js/2.0558cc01.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MyResume/static/js/2.0558cc01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34b5d3882cda677bcd47",
+    "url": "/MyResume/static/js/main.f45f4d61.chunk.js"
+  },
+  {
+    "revision": "b34e950eca970baae910",
+    "url": "/MyResume/static/js/runtime-main.58c46158.js"
+  }
+]);
